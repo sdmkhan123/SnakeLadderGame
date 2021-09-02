fae19646d550a8db9e55a4039c02ffc24b62bb63
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-02 23:02:31 +0530

    Move option forward, backward and stay same

--- Program.cs
+++ Program.cs
@@ -4,12 +4,27 @@ namespace SnakeLadderGame
 {
     class Program
     {
+        public const int win = 100;
+        public const int ladder = 1;
+        public const int snake = 2;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Game");
             int position = 0;
             Random random = new Random();
             int diceOutcome = random.Next(1, 7);
+            int checkOptions = random.Next(0, 3);
+            switch (checkOptions)
+            {
+                case ladder:
+                    position += diceOutcome;
+                    break;
+                case snake:
+                    position -= diceOutcome;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
